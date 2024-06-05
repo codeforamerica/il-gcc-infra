562fd1a65d16aa4d8c87641b28caf06a3d792bc9
James Armes (they/them) <jarmes@codeforamerica.org>
2024-06-05 11:59:11 -0400

    CCAP-137: Added resources to deploy the Document Transfer service. (#2)
    * Ignore pinned module warnings (only for our own modules)

--- tofu/config/staging/main.tf
+++ tofu/config/staging/main.tf
@@ -14,3 +14,52 @@ module "backend" {
   project     = "illinois-getchildcare"
   environment = "staging"
 }
+
+# Create an S3 bucket and KMS key for logging.
+module "logging" {
+  # tflint-ignore: terraform_module_pinned_source
+  source = "github.com/codeforamerica/tofu-modules/aws/logging"
+
+  project     = "illinois-getchildcare"
+  environment = "staging"
+}
+
+# Create a VPC with public and private subnets. Since this is a staging
+# environment, we'll use a single NAT gateway to reduce costs.
+module "vpc" {
+  # tflint-ignore: terraform_module_pinned_source
+  source = "github.com/codeforamerica/tofu-modules/aws/vpc"
+
+  cidr               = "10.0.20.0/22"
+  project            = "illinois-getchildcare"
+  environment        = "staging"
+  single_nat_gateway = true
+  logging_key_id     = module.logging.kms_key_arn
+
+  private_subnets = ["10.0.22.0/26", "10.0.22.64/26", "10.0.22.128/26"]
+  public_subnets  = ["10.0.20.0/26", "10.0.20.64/26", "10.0.20.128/26"]
+}
+
+# Deploy the Document Transfer service to a Fargate cluster.
+module "document_transfer" {
+  # tflint-ignore: terraform_module_pinned_source
+  source = "github.com/codeforamerica/tofu-modules/aws/fargate_service"
+
+  project         = "illinois-getchildcare"
+  project_short   = "il-gcc"
+  environment     = "staging"
+  service         = "document-transfer"
+  service_short   = "doc-trans"
+  domain          = "staging.document-transfer.cfa.codes"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  public_subnets  = module.vpc.public_subnets
+  logging_key_id  = module.logging.kms_key_arn
+  container_port  = 3000
+  force_delete    = true
+}
+
+# Display commands to push the Docker image to ECR.
+output "document_transfer_docker_push" {
+  value = module.document_transfer.docker_push
+}
