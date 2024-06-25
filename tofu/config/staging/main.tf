64f5e8d8e4f2e0c6a7e102f21c613fcf208280fb
James Armes (they/them) <jarmes@codeforamerica.org>
2024-06-25 16:26:26 -0400

    CCAP-141: Permit access to the microservice from the webapp only. (#3)

--- tofu/config/staging/main.tf
+++ tofu/config/staging/main.tf
@@ -38,6 +38,15 @@ module "vpc" {
 
   private_subnets = ["10.0.22.0/26", "10.0.22.64/26", "10.0.22.128/26"]
   public_subnets  = ["10.0.20.0/26", "10.0.20.64/26", "10.0.20.128/26"]
+
+  peers = {
+    "aptible" : {
+      account_id : "916150859591",
+      vpc_id : "vpc-041ed50bbc0467be5",
+      region : "us-east-1",
+      cidr : "10.226.0.0/16"
+    }
+  }
 }
 
 # Deploy the Document Transfer service to a Fargate cluster.
@@ -45,18 +54,32 @@ module "document_transfer" {
   # tflint-ignore: terraform_module_pinned_source
   source = "github.com/codeforamerica/tofu-modules/aws/fargate_service"
 
-  project         = "illinois-getchildcare"
-  project_short   = "il-gcc"
-  environment     = "staging"
-  service         = "document-transfer"
-  service_short   = "doc-trans"
-  domain          = "staging.document-transfer.cfa.codes"
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
-  public_subnets  = module.vpc.public_subnets
-  logging_key_id  = module.logging.kms_key_arn
-  container_port  = 3000
-  force_delete    = true
+  project                = "illinois-getchildcare"
+  project_short          = "il-gcc"
+  environment            = "staging"
+  service                = "document-transfer"
+  service_short          = "doc-trans"
+  domain                 = "staging.document-transfer.cfa.codes"
+  vpc_id                 = module.vpc.vpc_id
+  private_subnets        = module.vpc.private_subnets
+  public_subnets         = module.vpc.public_subnets
+  logging_key_id         = module.logging.kms_key_arn
+  container_port         = 3000
+  force_delete           = true
+  image_tags_mutable     = true
+  enable_execute_command = true
+
+  # Only allow access from the web application and its workers.
+  ingress_cidrs = ["10.226.0.0/16"]
+
+  environment_variables = {
+    RACK_ENV                    = "staging"
+    OTEL_EXPORTER_OTLP_ENDPOINT = "http://localhost:4318"
+  }
+}
+
+output "peer_ids" {
+  value = module.vpc.peer_ids
 }
 
 # Display commands to push the Docker image to ECR.
