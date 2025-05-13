b76dad9dbb70a5e5a8da15991fb6ebb8ef8b961c
spokenbird <agonzalez@codeforamerica.org>
2025-05-13 11:40:29 -0700

    [CCAP-690] Add back datadog config

--- tofu/modules/il_gcc_application/local.tf
+++ tofu/modules/il_gcc_application/local.tf
@@ -1,6 +1,6 @@
-# locals {
-#   datadog_lambda = [
-#     for lambda in data.aws_lambda_functions.all.function_names :
-#     lambda if length(regexall("^DatadogIntegration-ForwarderStack-", lambda)) > 0
-#   ]
-# }
+locals {
+  datadog_lambda = [
+    for lambda in data.aws_lambda_functions.all.function_names :
+    lambda if length(regexall("^DatadogIntegration-ForwarderStack-", lambda)) > 0
+  ]
+}
