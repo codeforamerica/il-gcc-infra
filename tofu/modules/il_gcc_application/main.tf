d96ad2fd30cb877fc3f568e235743af84cf687df
Marc P <mperlman@codeforamerica.org>
2025-07-15 19:42:52 -0500

    [CCAP-1155] adding new PDF modification flag support (#25)

--- tofu/modules/il_gcc_application/main.tf
+++ tofu/modules/il_gcc_application/main.tf
@@ -114,6 +114,7 @@ module "secrets" {
         enable_resource_org_email        = ""
         enable_sendgrid_email_validation = ""
         enable_faster_application_expiry = ""
+        allow_pdf_modification           = ""
         resource_org_emails              = ""
       })
     }
@@ -214,6 +215,7 @@ module "service" {
     ENABLE_RESOURCE_ORG_EMAIL          = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_resource_org_email"
     ENABLE_SENDGRID_EMAIL_VALIDATION   = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_sendgrid_email_validation"
     ENABLE_FASTER_APPLICATION_EXPIRY   = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_faster_application_expiry"
+    ALLOW_PDF_MODIFICATION             = "${module.secrets.secrets["il-gcc"].secret_arn}:allow_pdf_modification"
     RESOURCE_ORG_EMAILS                = "${module.secrets.secrets["il-gcc"].secret_arn}:resource_org_emails"
     AWS_BUCKET                         = "${module.secrets.secrets["aws"].secret_arn}:aws_bucket"
     AWS_REGION                         = "${module.secrets.secrets["aws"].secret_arn}:aws_region"
