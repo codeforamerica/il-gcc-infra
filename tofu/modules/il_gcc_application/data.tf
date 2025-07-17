f14f9909ad04ae66d30191fc655bce0df727b792
spokenbird <agonzalez@codeforamerica.org>
2025-07-17 15:16:40 -0700

    Remove duplicate data configuration

--- tofu/modules/il_gcc_application/data.tf
+++ tofu/modules/il_gcc_application/data.tf
@@ -1,7 +1,3 @@
-# AWS account and partition information for bucket policy
-data "aws_caller_identity" "identity" {}
-data "aws_partition" "current" {}
-
 # Find the lambda function for the Datadog forwarder so that we can use it as a
 # destination for CloudWatch log subscriptions.
 data "aws_lambda_functions" "all" {}
