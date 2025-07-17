37cb081875e30e7917bde60371e9a138978940d3
Marc P <mperlman@codeforamerica.org>
2025-07-17 10:26:50 -0500

    Creating dashboard for QA (#26)

--- tofu/modules/il_gcc_application/main.tf
+++ tofu/modules/il_gcc_application/main.tf
@@ -165,6 +165,7 @@ module "service" {
   container_port         = 8080
   memory                 = 2048
   force_delete           = var.force_delete
+  container_command      = ["./scripts/webapp_launcher.sh"]
   create_endpoint        = true
   image_tags_mutable     = true
   version_parameter      = aws_ssm_parameter.version.name
@@ -241,7 +242,6 @@ module "worker" {
   enable_execute_command = true
   create_endpoint        = false
   create_repository      = false
-  container_command      = ["./script/worker", "run"]
   image_url              = module.service.repository_url
   version_parameter      = aws_ssm_parameter.version.name
   repository_arn         = module.service.repository_arn
@@ -249,6 +249,130 @@ module "worker" {
   environment_variables = {
     DATABASE_HOST = module.database.cluster_endpoint
     AWS_BUCKET    = "get-child-care-illinois-${var.environment}"
+  }
+
+  environment_secrets = {
+    DATABASE_PASSWORD                  = "${module.database.secret_arn}:password"
+    DATABASE_USER                      = "${module.database.secret_arn}:username"
+    SENDGRID_API_KEY                   = "${module.secrets.secrets["sendgrid"].secret_arn}:sendgrid_api_key"
+    SENDGRID_PUBLIC_KEY                = "${module.secrets.secrets["sendgrid"].secret_arn}:sendgrid_public_key"
+    SENDGRID_EMAIL_VALIDATION_API_KEY  = "${module.secrets.secrets["sendgrid"].secret_arn}:sendgrid_email_validation_api_key"
+    SMARTY_AUTH_ID                     = "${module.secrets.secrets["smarty"].secret_arn}:auth_id"
+    SMARTY_AUTH_TOKEN                  = "${module.secrets.secrets["smarty"].secret_arn}:auth_token"
+    MIXPANEL_API_KEY                   = "${module.secrets.secrets["mixpanel"].secret_arn}:api_key"
+    ENCRYPTION_KEY                     = "${module.secrets.secrets["google"].secret_arn}:encryption_key"
+    DATADOG_API_KEY                    = "${module.secrets.secrets["datadog"].secret_arn}:api_key"
+    DATADOG_APPLICATION_KEY            = "${module.secrets.secrets["datadog"].secret_arn}:app_key"
+    DATADOG_SESSION_REPLAY_SAMPLE_RATE = "${module.secrets.secrets["datadog"].secret_arn}:session_replay_sample_rate"
+    DATADOG_RUM_APPLICATION_ID         = "${module.secrets.secrets["datadog"].secret_arn}:rum_app_id"
+    DATADOG_RUM_CLIENT_TOKEN           = "${module.secrets.secrets["datadog"].secret_arn}:rum_client_token"
+    DATADOG_ENVIRONMENT                = "${module.secrets.secrets["datadog"].secret_arn}:environment"
+    SENTRY_DSN                         = "${module.secrets.secrets["sentry"].secret_arn}:dsn"
+    JOBRUNR_DASHBOARD_ENABLED          = "${module.secrets.secrets["jobrunr"].secret_arn}:dashboard_enabled"
+    ENABLE_BACKGROUND_JOBS_FLAG        = "${module.secrets.secrets["jobrunr"].secret_arn}:enable_background_jobs"
+    OCP_APIM_SUBSCRIPTION_KEY          = "${module.secrets.secrets["ccms"].secret_arn}:ocp_apim_key"
+    CCMS_API_BASE_URL                  = "${module.secrets.secrets["ccms"].secret_arn}:api_base_url"
+    CCMS_API_USERNAME                  = "${module.secrets.secrets["ccms"].secret_arn}:api_username"
+    CCMS_API_PASSWORD                  = "${module.secrets.secrets["ccms"].secret_arn}:api_password"
+    CCMS_TRANSACTION_DELAY_MINUTES     = "${module.secrets.secrets["ccms"].secret_arn}:transaction_delay"
+    ENABLE_CCMS_INTEGRATION            = "${module.secrets.secrets["ccms"].secret_arn}:enable_integration"
+    CCMS_OFFLINE_TIME_RANGES           = "${module.secrets.secrets["ccms"].secret_arn}:ccms_offline_time_ranges"
+    CONVERT_UPLOADS_TO_PDF             = "${module.secrets.secrets["il-gcc"].secret_arn}:convert_uploads_to_pdf"
+    CONVERTED_FILE_SUFFIX              = "${module.secrets.secrets["il-gcc"].secret_arn}:converted_file_suffix"
+    SPRING_PROFILES_ACTIVE             = "${module.secrets.secrets["il-gcc"].secret_arn}:spring_profiles_active"
+    ACTIVE_CASELOAD_CODES              = "${module.secrets.secrets["il-gcc"].secret_arn}:active_caseload_codes"
+    PENDING_CASELOAD_CODES             = "${module.secrets.secrets["il-gcc"].secret_arn}:pending_caseload_codes"
+    ENABLE_NEW_SDA_CASELOAD_CODES      = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_new_sda_caseload_codes"
+    ADDRESS_VALIDATION_ENABLED         = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_address_validation"
+    ENABLE_EMAILS_FLAG                 = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_emails"
+    ENABLE_MULTIPLE_PROVIDERS          = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_multiple_providers"
+    ENABLE_RESOURCE_ORG_EMAIL          = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_resource_org_email"
+    ENABLE_SENDGRID_EMAIL_VALIDATION   = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_sendgrid_email_validation"
+    ENABLE_FASTER_APPLICATION_EXPIRY   = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_faster_application_expiry"
+    ALLOW_PDF_MODIFICATION             = "${module.secrets.secrets["il-gcc"].secret_arn}:allow_pdf_modification"
+    RESOURCE_ORG_EMAILS                = "${module.secrets.secrets["il-gcc"].secret_arn}:resource_org_emails"
+    AWS_BUCKET                         = "${module.secrets.secrets["aws"].secret_arn}:aws_bucket"
+    AWS_REGION                         = "${module.secrets.secrets["aws"].secret_arn}:aws_region"
+    AWS_SECRET_KEY                     = "${module.secrets.secrets["aws"].secret_arn}:aws_secret_key"
+    AWS_ACCESS_KEY                     = "${module.secrets.secrets["aws"].secret_arn}:aws_access_key"
+  }
+
+  tags = { service = "application-worker" }
+}
+
+module "dashboard" {
+  source = "github.com/codeforamerica/tofu-modules-aws-fargate-service?ref=1.5.0"
+
+  project                = "illinois-getchildcare"
+  project_short          = "il-gcc"
+  stats_prefix           = "illinois-getchildcare/qa"
+  environment            = var.environment
+  service                = "dashboard"
+  service_short          = "dshbd"
+  vpc_id                 = var.vpc_id
+  private_subnets        = var.private_subnets
+  logging_key_id         = var.logging_key
+  force_delete           = var.force_delete
+  container_port         = 8000
+  memory                 = 2048
+  enable_execute_command = true
+  create_endpoint        = false
+  create_repository      = false
+  container_command      = ["./scripts/jobrunr_dashboard_launcher.sh"]
+  image_url              = module.service.repository_url
+  version_parameter      = aws_ssm_parameter.version.name
+  repository_arn         = module.service.repository_arn
+  public                 = true
+  
+  environment_variables = {
+    DATABASE_HOST = module.database.cluster_endpoint
+    AWS_BUCKET    = "get-child-care-illinois-${var.environment}"
+  }
+
+  environment_secrets = {
+    DATABASE_PASSWORD                  = "${module.database.secret_arn}:password"
+    DATABASE_USER                      = "${module.database.secret_arn}:username"
+    SENDGRID_API_KEY                   = "${module.secrets.secrets["sendgrid"].secret_arn}:sendgrid_api_key"
+    SENDGRID_PUBLIC_KEY                = "${module.secrets.secrets["sendgrid"].secret_arn}:sendgrid_public_key"
+    SENDGRID_EMAIL_VALIDATION_API_KEY  = "${module.secrets.secrets["sendgrid"].secret_arn}:sendgrid_email_validation_api_key"
+    SMARTY_AUTH_ID                     = "${module.secrets.secrets["smarty"].secret_arn}:auth_id"
+    SMARTY_AUTH_TOKEN                  = "${module.secrets.secrets["smarty"].secret_arn}:auth_token"
+    MIXPANEL_API_KEY                   = "${module.secrets.secrets["mixpanel"].secret_arn}:api_key"
+    ENCRYPTION_KEY                     = "${module.secrets.secrets["google"].secret_arn}:encryption_key"
+    DATADOG_API_KEY                    = "${module.secrets.secrets["datadog"].secret_arn}:api_key"
+    DATADOG_APPLICATION_KEY            = "${module.secrets.secrets["datadog"].secret_arn}:app_key"
+    DATADOG_SESSION_REPLAY_SAMPLE_RATE = "${module.secrets.secrets["datadog"].secret_arn}:session_replay_sample_rate"
+    DATADOG_RUM_APPLICATION_ID         = "${module.secrets.secrets["datadog"].secret_arn}:rum_app_id"
+    DATADOG_RUM_CLIENT_TOKEN           = "${module.secrets.secrets["datadog"].secret_arn}:rum_client_token"
+    DATADOG_ENVIRONMENT                = "${module.secrets.secrets["datadog"].secret_arn}:environment"
+    SENTRY_DSN                         = "${module.secrets.secrets["sentry"].secret_arn}:dsn"
+    JOBRUNR_DASHBOARD_ENABLED          = "${module.secrets.secrets["jobrunr"].secret_arn}:dashboard_enabled"
+    ENABLE_BACKGROUND_JOBS_FLAG        = "${module.secrets.secrets["jobrunr"].secret_arn}:enable_background_jobs"
+    OCP_APIM_SUBSCRIPTION_KEY          = "${module.secrets.secrets["ccms"].secret_arn}:ocp_apim_key"
+    CCMS_API_BASE_URL                  = "${module.secrets.secrets["ccms"].secret_arn}:api_base_url"
+    CCMS_API_USERNAME                  = "${module.secrets.secrets["ccms"].secret_arn}:api_username"
+    CCMS_API_PASSWORD                  = "${module.secrets.secrets["ccms"].secret_arn}:api_password"
+    CCMS_TRANSACTION_DELAY_MINUTES     = "${module.secrets.secrets["ccms"].secret_arn}:transaction_delay"
+    ENABLE_CCMS_INTEGRATION            = "${module.secrets.secrets["ccms"].secret_arn}:enable_integration"
+    CCMS_OFFLINE_TIME_RANGES           = "${module.secrets.secrets["ccms"].secret_arn}:ccms_offline_time_ranges"
+    CONVERT_UPLOADS_TO_PDF             = "${module.secrets.secrets["il-gcc"].secret_arn}:convert_uploads_to_pdf"
+    CONVERTED_FILE_SUFFIX              = "${module.secrets.secrets["il-gcc"].secret_arn}:converted_file_suffix"
+    SPRING_PROFILES_ACTIVE             = "${module.secrets.secrets["il-gcc"].secret_arn}:spring_profiles_active"
+    ACTIVE_CASELOAD_CODES              = "${module.secrets.secrets["il-gcc"].secret_arn}:active_caseload_codes"
+    PENDING_CASELOAD_CODES             = "${module.secrets.secrets["il-gcc"].secret_arn}:pending_caseload_codes"
+    ENABLE_NEW_SDA_CASELOAD_CODES      = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_new_sda_caseload_codes"
+    ADDRESS_VALIDATION_ENABLED         = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_address_validation"
+    ENABLE_EMAILS_FLAG                 = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_emails"
+    ENABLE_MULTIPLE_PROVIDERS          = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_multiple_providers"
+    ENABLE_RESOURCE_ORG_EMAIL          = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_resource_org_email"
+    ENABLE_SENDGRID_EMAIL_VALIDATION   = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_sendgrid_email_validation"
+    ENABLE_FASTER_APPLICATION_EXPIRY   = "${module.secrets.secrets["il-gcc"].secret_arn}:enable_faster_application_expiry"
+    ALLOW_PDF_MODIFICATION             = "${module.secrets.secrets["il-gcc"].secret_arn}:allow_pdf_modification"
+    RESOURCE_ORG_EMAILS                = "${module.secrets.secrets["il-gcc"].secret_arn}:resource_org_emails"
+    AWS_BUCKET                         = "${module.secrets.secrets["aws"].secret_arn}:aws_bucket"
+    AWS_REGION                         = "${module.secrets.secrets["aws"].secret_arn}:aws_region"
+    AWS_SECRET_KEY                     = "${module.secrets.secrets["aws"].secret_arn}:aws_secret_key"
+    AWS_ACCESS_KEY                     = "${module.secrets.secrets["aws"].secret_arn}:aws_access_key"
   }
 
   tags = { service = "application-worker" }
