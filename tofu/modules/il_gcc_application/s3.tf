c95a0aba85aedfda8dc9249b5407da355b96fe83
spokenbird <agonzalez@codeforamerica.org>
2025-07-18 10:53:19 -0700

    Update to use KMS key encryption

--- tofu/modules/il_gcc_application/s3.tf
+++ tofu/modules/il_gcc_application/s3.tf
@@ -23,7 +23,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "get_child_care_il
     bucket_key_enabled = true
 
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = aws_kms_key.get_child_care_illinois.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
